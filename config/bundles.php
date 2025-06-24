2414ccc31256cddcb559863f2047bcbac25d93c6
tbeaumont79 <tbeaumont79@icloud.com>
2025-06-24 13:32:06 +0200

    feat: adding a HomeController + twig template

--- config/bundles.php
+++ config/bundles.php
@@ -5,4 +5,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
+    Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
 ];
--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
 import { defineConfig } from "vite";
 import symfonyPlugin from "vite-plugin-symfony";
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from "@tailwindcss/vite";
 
 /* if you're using React */
 // import react from '@vitejs/plugin-react';
@@ -9,11 +9,7 @@ export default defineConfig({
     plugins: [
         /* react(), // if you're using React */
         symfonyPlugin({
-            stimulus: true,
             stimulus: "./assets/other-dir/controllers.json",
-            stimulus: {
-                fetchMode: "lazy",
-            },
         }),
         tailwindcss(),
     ],
