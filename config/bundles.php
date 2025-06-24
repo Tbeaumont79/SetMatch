958b0e75a1f83fb290d0cef5a84f500cb49f9d57
tbeaumont79 <tbeaumont79@icloud.com>
2025-06-24 17:28:09 +0200

    feat: adding security + ORM + User entity

--- config/bundles.php
+++ config/bundles.php
@@ -6,4 +6,7 @@ return [
     Pentatrion\ViteBundle\PentatrionViteBundle::class => ['all' => true],
     Symfony\UX\StimulusBundle\StimulusBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
+    Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
+    Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
+    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
 ];
