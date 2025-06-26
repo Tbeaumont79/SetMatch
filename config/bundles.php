aa1b61ca90a6278093e0a32c1cd224eed4f11b08
tbeaumont79 <tbeaumont79@icloud.com>
2025-06-26 20:45:15 +0200

    feat: adding liip focusing on the resizing images

--- config/bundles.php
+++ config/bundles.php
@@ -14,4 +14,5 @@ return [
     Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Liip\ImagineBundle\LiipImagineBundle::class => ['all' => true],
 ];
