45a89b6f3f4c678741c06774c2eef58efee623bc
tbeaumont79 <tbeaumont79@icloud.com>
2025-06-26 20:15:27 +0200

    feat: update the ui for the user posts

--- src/Controller/UserPostsController.php
+++ src/Controller/UserPostsController.php
@@ -5,16 +5,16 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use App\Entity\User;
+use App\Entity\Post;
+use Doctrine\ORM\EntityManagerInterface;
 
 final class UserPostsController extends AbstractController
 {
     #[Route('/user/posts', name: 'app_user_posts')]
-    public function index(): Response
+    public function index(EntityManagerInterface $entityManager): Response
     {
+        $userPost = $entityManager->getRepository(Post::class)->findBy(['author' => $this->getUser()]);
 
-        $user = new User();
-        
         return $this->render('user_posts/index.html.twig', [
             'controller_name' => 'UserPostsController',
             'userPost' => $userPost
